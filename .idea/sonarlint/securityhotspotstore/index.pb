ba6523ea95f95cc944a960b2d500c4769d7c46fe
RuokeZhang <zhang.12783@osu.edu>
2023-11-12 19:27:54 -0500

    initialize

--- .idea/sonarlint/securityhotspotstore/index.pb
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -1,3 +1,5 @@
 
 G
 banking-service/pom.xml,e/1/e1e93b3ff76484e53c0fbae5d2269428c6a9618f
+H
+shipping-service/pom.xml,a/1/a1613f07c4b8b3b3baa4105366d2147130d039ee
---
+++ shipping-service/src/test/java/com/urbanthreads/shippingservice/ShippingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbanthreads.shippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShippingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
